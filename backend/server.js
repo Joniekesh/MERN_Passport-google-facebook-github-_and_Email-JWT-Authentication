4ab1a2c37917096afe3bf6a01bba0b2f8e61d27b
Okoro John Darlington <joniekesh@gmail.com>
2022-02-09 21:15:46 +0100

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,52 @@
-require('dotenv').config()
-const express = require('express')
-const passport = require('passport')
-const cookieSession = require('cookie-session')
-const authRoutes = require('./routes/auth')
-const connectDB = require('./config/db')
-require('./config/passport')(passport)
-const cors = require('cors')
+require("dotenv").config();
+const express = require("express");
+const passport = require("passport");
+const cookieSession = require("cookie-session");
+const authRoutes = require("./routes/auth");
+const connectDB = require("./config/db");
+require("./config/passport")(passport);
+const cors = require("cors");
+const path = require("path");
 
-
-
-const app = express()
+const app = express();
 
 // Connect to Database
-connectDB()
+connectDB();
 
 // Middlewares
-app.use(express.json())
-
-app.use(cookieSession({
-    maxAge: 24 * 60 * 60 * 1000,
-    keys: ['123abc']
-}))
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(express.json());
 
 app.use(
-    cors({
-      origin: "http://localhost:3000",
-      methods: "GET,POST,PUT,DELETE",
-      credentials: true,
-    })
-  );
+	cookieSession({
+		maxAge: 24 * 60 * 60 * 1000,
+		keys: ["123abc"],
+	})
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
+app.use(
+	cors({
+		origin: "http://localhost:3000",
+		methods: "GET,POST,PUT,DELETE",
+		credentials: true,
+	})
+);
 
 //APIS
-app.use('/auth', authRoutes)
+app.use("/auth", authRoutes);
+const _dirname = path.resolve();
 
-const PORT = process.env.PORT || 5000
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(_dirname, "/frontend/build")));
 
-app.listen(PORT, () => console.log(`SERVER running in ${process.env.NODE_ENV} MODE on PORT ${PORT}`))
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(_dirname, "frontend", "build", "index.html"));
+	});
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+	console.log(`SERVER running in ${process.env.NODE_ENV} MODE on PORT ${PORT}`)
+);
